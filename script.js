cac87277cd09dfd7c8875a66a8e2ea57db316680
SriMoorthy <srimoorthy27@gmail.com>
2024-09-05 22:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+function getData() {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const numbers = [1,2,3,4]
+			resolve(numbers)
+			filterEvenNum()
+		},3000)
+	})
+}
+
+function filterEvenNum(numbers) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const evenNum = numbers.filter(num => num % 2 == 0)
+			document.getElimentById("output").textcontent = evenNum.join(',')
+			resolve(evenNum);
+			multipleByTwo()
+		},1000)
+	})
+}
+
+function multipleByTwo(evenNum) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			const multi = evenNum.map(num => num * 2)
+			document.getElimentById("output").textcontent = multi.join(',')
+			resolve(multi)
+		},2000)
+	})
+}
+
+getData();
